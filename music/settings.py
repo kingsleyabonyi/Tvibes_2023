114fa09ac5ef1761d2a8b8da60143d7c5db50c97
Kingsley <kinabonyi@gmail.com>
2023-09-30 04:13:16 +0100

    modified settings

--- music/settings.py
+++ music/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x2q_&qq6e^2wup3h)^+ixu6_=qc@#24_jeijoz@18l-&oqo(&3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tvibes.onrender.com', 'http://127.0.0.1:8000/']
+ALLOWED_HOSTS = ['tvibes.onrender.com', 'http://127.0.0.1:8000/', '*']
 
 
 # Application definition
